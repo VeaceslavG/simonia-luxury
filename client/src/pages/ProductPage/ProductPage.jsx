0cf009e40f1ddf8e657675c7281b6bbc627f35ff
VeaceslavG <veaceslav.gorbuleac019@gmail.com>
2026-01-01 13:31:44 +0200

    fix: import Helmet in components to fix ReferenceError

--- client/src/pages/ProductPage/ProductPage.jsx
+++ client/src/pages/ProductPage/ProductPage.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useCart } from "../../context/CartContext";
 import { toast } from "react-toastify";
+import { Helmet } from "react-helmet-async";
 import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./productPage.scss";
